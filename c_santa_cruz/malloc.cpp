2ff95449d36020753ad3eb9ff9726fe634aa69ea
vaibhavsd <vd99099@gmail.com>
2017-08-08 13:33:45 -0700

    corrected malloc code

--- c_santa_cruz/malloc.cpp
+++ c_santa_cruz/malloc.cpp
@@ -16,15 +16,16 @@ int main() {
     delete []s; //delete an array
     delete p; //delete single element
 
-    /*
+
     //Verify this
     // Creating a multi-dimensional array using C
+    int nrows =6; int ncolumns= 6;
     int **array;
-    array = malloc(nrows* sizeof(int *));
+    array = (int**)malloc(nrows* sizeof(int *));
 
     for(int i = 0; i < nrows; i++){
-        array[i] = malloc(ncolumns * sizeof(int));
+        array[i] = (int*)malloc(ncolumns * sizeof(int));
     }
-    */
+
 
 }
